2bf734605dbaa8aa1ea77b9037948a30aa90198d
MrDemonid <MrDemonid@yandex.ru>
2025-01-23 21:47:32 +0300

    add tei module test for Storage-service

---
+++ Storage-service/src/test/java/mr/demonid/storage/service/ObjectEntityServiceTest.java
@@ -0,0 +1,186 @@
+package mr.demonid.storage.service;
+
+import mr.demonid.storage.service.domain.ObjectEntity;
+import mr.demonid.storage.service.dto.ObjectEntityDTO;
+import mr.demonid.storage.service.exceptions.BadObjectException;
+import mr.demonid.storage.service.exceptions.BadPersonException;
+import mr.demonid.storage.service.makers.ObjectMaker;
+import mr.demonid.storage.service.repository.ObjectEntityRepository;
+import mr.demonid.storage.service.services.ObjectEntityService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+
+/**
+ * Модульные тесты.
+ */
+@ExtendWith(MockitoExtension.class)
+public class ObjectEntityServiceTest {
+
+    @Mock
+    private ObjectEntityRepository objectRepository;
+
+    @Mock
+    private ObjectMaker objectMaker;
+
+    @InjectMocks
+    private ObjectEntityService objectEntityService;
+
+    ObjectEntityDTO dto;
+    ObjectEntityDTO dtoNull;
+    ObjectEntity objectEntity;
+    ObjectEntity objectEntityNull;
+    /**
+     * Подготавливаем данные для каждого тестового метода.
+     */
+    @BeforeEach
+    public void setup() {
+        dto = new ObjectEntityDTO(1L, "desc", "address", List.of(), Set.of(), Set.of(), Set.of());
+        dtoNull = new ObjectEntityDTO(null, "desc", "address", List.of(), Set.of(), Set.of(), Set.of());
+        objectEntity = new ObjectEntity(1L, "desc", "address", List.of(), Set.of(), Set.of(), Set.of());
+        objectEntityNull = new ObjectEntity(null, "desc", "address", List.of(), Set.of(), Set.of(), Set.of());
+    }
+
+    /**
+     * Сервис создания нового объекта.
+     * Тестируем с корректным объектом.
+     */
+    @Test
+    void createObjectEntityWithCorrectParams() {
+        // ставим заглушки
+        when(objectRepository.existsById(dto.getId())).thenReturn(false);
+        when(objectRepository.save(any(ObjectEntity.class))).thenReturn(objectEntity);
+        when(objectMaker.toDto(any(ObjectEntity.class))).thenReturn(dto);
+        when(objectMaker.toObject(any(ObjectEntityDTO.class))).thenReturn(objectEntity);
+
+        assertDoesNotThrow(() -> objectEntityService.create(dto));
+        verify(objectRepository).save(any(ObjectEntity.class));
+    }
+
+    /**
+     * Сервис создания нового объекта.
+     * Тестируем с нулевым ID.
+     */
+    @Test
+    void createObjectEntityWithBadId() {
+        // ставим заглушки
+        assertThrows(BadPersonException.class, () -> objectEntityService.create(dtoNull));
+    }
+
+    /**
+     * Сервис создания нового объекта.
+     * Тестируем с уже существующим пользователем в БД.
+     */
+    @Test
+    void createObjectEntityWithUserPresent() {
+        // ставим заглушки
+        when(objectRepository.existsById(dto.getId())).thenReturn(true);
+        assertThrows(BadObjectException.class, () -> objectEntityService.create(dto));
+    }
+
+    /**
+     * Сервис создания нового объекта.
+     * Тестируем с корректным объектом, но с ошибкой записи в БД.
+     */
+    @Test
+    void createObjectEntityWithExternalErrorOnDBSave() {
+        // ставим заглушки
+        when(objectRepository.existsById(any())).thenReturn(false);
+        when(objectRepository.save(any(ObjectEntity.class))).thenThrow(RuntimeException.class); // запись вызовет исключение
+        when(objectMaker.toDto(any(ObjectEntity.class))).thenReturn(dto);
+
+        assertThrows(BadObjectException.class, () -> objectEntityService.create(dto));
+    }
+
+    /*
+        =============================================================================================
+        Сервис обновления данных объекта.
+        =============================================================================================
+     */
+
+    /**
+     * Тестируем с корректными данными.
+     *
+     */
+    @Test
+    void updateObjectEntityWithCorrectParams() {
+        when(objectRepository.findObjectDetails(dto.getId())).thenReturn(Optional.of(objectEntity));
+        when(objectRepository.save(any(ObjectEntity.class))).thenReturn(objectEntity);
+        when(objectMaker.toDto(any(ObjectEntity.class))).thenReturn(dto);
+        when(objectMaker.toObject(any(ObjectEntityDTO.class))).thenReturn(objectEntity);
+
+        assertDoesNotThrow(() -> objectEntityService.update(dto));
+        verify(objectRepository).save(any(ObjectEntity.class));
+    }
+
+    /**
+     * Тестируем с несуществующим объектом.
+     */
+    @Test
+    void updateObjectEntityWithBadId() {
+        when(objectRepository.findObjectDetails(any())).thenReturn(Optional.empty());
+        assertThrows(BadObjectException.class, () -> objectEntityService.update(dtoNull));
+    }
+
+    /**
+     * Тестируем с корректным объектом, но с ошибкой в БД.
+     */
+    @Test
+    void updateObjectEntityWithExternalErrorOnDBUpdate() {
+        when(objectRepository.findObjectDetails(dto.getId())).thenReturn(Optional.of(objectEntity));
+        when(objectRepository.save(any(ObjectEntity.class))).thenThrow(RuntimeException.class);
+        when(objectMaker.toDto(any(ObjectEntity.class))).thenReturn(dto);
+
+        assertThrows(BadObjectException.class, () -> objectEntityService.update(dtoNull));
+    }
+
+    /*
+        =============================================================================================
+        Удаление объекта
+        =============================================================================================
+      */
+
+    /**
+     * Тестируем с корректными данными.
+     */
+    @Test
+    void deleteObjectEntityWithCorrectParams() {
+        when(objectRepository.existsById(dto.getId())).thenReturn(true);
+        doNothing().when(objectRepository).deleteById(dto.getId());
+
+        assertDoesNotThrow(() -> objectEntityService.delete(dto.getId()));
+        verify(objectRepository).deleteById(dto.getId());
+    }
+
+    /**
+     * Тестируем с некорректным Id.
+     */
+    @Test
+    void deleteObjectEntityWithBadId() {
+        when(objectRepository.existsById(any())).thenReturn(false);
+        assertThrows(BadObjectException.class, () -> objectEntityService.delete(dtoNull.getId()));
+    }
+
+    /**
+     * Тестируем с корректным объектом, но с ошибкой в БД.
+     */
+    @Test
+    void deleteObjectEntityWithExternalErrorOnDBDelete() {
+        when(objectRepository.existsById(dto.getId())).thenReturn(true);
+        doThrow(RuntimeException.class).when(objectRepository).deleteById(dto.getId());
+
+        assertThrows(BadObjectException.class, () -> objectEntityService.delete(dto.getId()));
+    }
+
+}
---
+++ Storage-service/src/test/java/mr/demonid/storage/service/WorkScheduleTest.java
@@ -0,0 +1,180 @@
+package mr.demonid.storage.service;
+
+import mr.demonid.storage.service.domain.ObjectEntity;
+import mr.demonid.storage.service.domain.WorkSchedule;
+import mr.demonid.storage.service.dto.ObjectEntityDTO;
+import mr.demonid.storage.service.dto.WorkScheduleDTO;
+import mr.demonid.storage.service.exceptions.BadScheduleException;
+import mr.demonid.storage.service.makers.WorkScheduleMaker;
+import mr.demonid.storage.service.repository.WorkScheduleRepository;
+import mr.demonid.storage.service.services.WorkScheduleService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+/**
+ * Модульные тесты.
+ */
+@ExtendWith(MockitoExtension.class)
+//@AutoConfigureMockMvc
+public class WorkScheduleTest {
+
+    @Mock
+    WorkScheduleMaker maker;
+    @Mock
+    WorkScheduleRepository repository;
+    @InjectMocks
+    WorkScheduleService service;
+
+    WorkScheduleDTO dto;
+    WorkScheduleDTO dtoNull;
+    WorkSchedule workSchedule;
+
+    /**
+     * Подготавливаем данные для каждого тестового метода.
+     */
+    @BeforeEach
+    public void setup() {
+        dto = new WorkScheduleDTO(1L, "schedule details", Set.of(), Set.of());
+        dtoNull = new WorkScheduleDTO(null, "schedule details", Set.of(), Set.of());
+        workSchedule = new WorkSchedule(1L, "schedule details", Set.of(), Set.of());
+    }
+
+
+    /*
+        ========================================================================
+        Добавление нового режима работы.
+        ========================================================================
+     */
+    /*
+     * Тестируем с корректными параметрами.
+     */
+    @Test
+    void createWorkScheduleWithCorrectData() {
+        when(maker.toSchedule(any(WorkScheduleDTO.class))).thenReturn(workSchedule);
+        when(repository.save(workSchedule)).thenReturn(workSchedule);
+        when(maker.toDTO(workSchedule)).thenReturn(dto);
+
+        assertDoesNotThrow(() -> service.create(dtoNull));
+        verify(repository).save(workSchedule);
+        verify(maker).toDTO(workSchedule);
+    }
+
+    /*
+     * Тестируем с неверным Id
+     */
+    @Test
+    void createWorkScheduleWithNullId() {
+        assertThrows(BadScheduleException.class, () -> service.create(dto));
+    }
+
+    /*
+     * Тестируем с ошибкой БД.
+     */
+    @Test
+    void updateWorkScheduleWithBadDBSave() {
+        when(maker.toSchedule(any(WorkScheduleDTO.class))).thenReturn(workSchedule);
+        when(repository.save(workSchedule)).thenThrow(BadScheduleException.class);
+
+        assertThrows(BadScheduleException.class, () -> service.create(dtoNull));
+        verify(repository).save(workSchedule);
+    }
+
+
+    /*
+        ========================================================================
+        Обновление существующего режима
+        ========================================================================
+     */
+    /*
+     * Тестируем с корректными параметрами.
+     */
+    @Test
+    void updateWorkScheduleWithCorrectData() {
+        when(repository.findById(workSchedule.getId())).thenReturn(Optional.of(workSchedule));
+        when(maker.toSchedule(any(WorkScheduleDTO.class))).thenReturn(workSchedule);
+        when(repository.save(workSchedule)).thenReturn(workSchedule);
+        when(maker.toDTO(workSchedule)).thenReturn(dto);
+
+        assertDoesNotThrow(() -> service.update(dto));
+        verify(repository).save(workSchedule);
+    }
+
+    /*
+     * Тестируем с неверным Id
+     */
+    @Test
+    void updateWorkScheduleWithNullId() {
+        when(repository.findById(workSchedule.getId())).thenReturn(Optional.empty());
+
+        assertThrows(BadScheduleException.class, () -> service.update(dto));
+    }
+
+    /*
+     * Тестируем с ошибкой БД.
+     */
+    @Test
+    void updateWorkScheduleWithBadDBUpdate() {
+        when(repository.findById(workSchedule.getId())).thenReturn(Optional.of(workSchedule));
+        when(maker.toSchedule(any(WorkScheduleDTO.class))).thenReturn(workSchedule);
+        when(repository.save(workSchedule)).thenThrow(BadScheduleException.class);
+
+        assertThrows(BadScheduleException.class, () -> service.update(dto));
+    }
+
+
+    /*
+        ========================================================================
+        Удаление существующего режима
+        ========================================================================
+     */
+    /*
+     * Тестируем с корректными параметрами.
+     */
+    @Test
+    void deleteWorkScheduleWithCorrectData() {
+        when(repository.findById(dto.getId())).thenReturn(Optional.of(workSchedule));
+
+        assertDoesNotThrow(() -> service.delete(dto.getId()));
+        verify(repository).deleteById(workSchedule.getId());
+    }
+
+    /*
+     * Тестируем с некорректным Id
+     */
+    @Test
+    void deleteWorkScheduleWithNullId() {
+        when(repository.findById(dto.getId())).thenReturn(Optional.empty());
+
+        assertThrows(BadScheduleException.class, () -> service.delete(dto.getId()));
+    }
+
+    /*
+     * Тестируем с корректным объектом, но с ошибкой в БД.
+     */
+    @Test
+    void deleteWorkScheduleWithBadDBDelete() {
+        when(repository.findById(dto.getId())).thenReturn(Optional.of(workSchedule));
+        doThrow(BadScheduleException.class).when(repository).deleteById(workSchedule.getId());
+
+        assertThrows(BadScheduleException.class, () -> service.delete(dto.getId()));
+        verify(repository).deleteById(workSchedule.getId());
+    }
+
+}
